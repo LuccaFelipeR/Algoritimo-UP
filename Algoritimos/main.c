f5e8e5bdbe9b875a5472d9879165e3d6c66d8727
WolffLoney <luccafelipe99@gmail.com>
2025-08-01 21:33:50 -0300

    Add files via upload

---
+++ Algoritimos/main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+ int main ()
+ {
+    int num1,num2,soma;
+
+    printf ("DIGITE O PRIMEIRO NUMERO: \n");
+    scanf ("%d", &num1);
+
+    printf ("DIGITE O SEGUNDO NUMERO: \n");
+    scanf ("%d", &num2);
+
+    soma = num1 + num2;
+
+    printf("\nRESULTADO DA SUA MISERAVEL SOMA FOI: %d\n", soma);
+
+
+
+     return 0;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+int main()
+{
+  int num;
+  char c1;
+  double d1;
+
+    printf ("DIGITE SEU CARACTERE DA SORTE: ");
+    scanf ("%c", &c1);
+    printf ("DIGITE SEU NUMERO DA SORTE: ");
+    scanf ("%d", &num);
+    printf ("DIGITE UM DECIMAL DO AZAR: ");
+    scanf ("%lf", &d1);
+
+
+    printf ("\nCARACTERE: %c \nNUMERO DA SORTE: %d \nDECIMAL DO AZAR: %.2lf\n", c1,num,d1);
+
+
+
+    return 0;
+}
+*/
