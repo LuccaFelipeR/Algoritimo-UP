9fa075c315d88454de2aa24868051530b22a343e
WolffLoney <luccafelipe99@gmail.com>
2025-08-13 21:43:44 -0300

    Add.

---
+++ Algoritimo atividade 13.08.25/main.c
@@ -0,0 +1,162 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+/*
+int main()
+{
+
+    double angulo, seno, cosseno, tangente;
+
+
+    printf ("Me informe um Valor do Angulo: ");
+    scanf ("%lf", &angulo);
+
+    seno = sin(angulo);
+    cosseno = cos (angulo);
+    tangente = tan (angulo);
+
+    printf ("Seno %.2lf\nCosseno: %.2lf\nTangente: %.2lf", seno, cosseno, tangente);
+
+
+    return 0;
+}
+*/
+/*
+int main ()
+{
+    double num, raiz;
+
+    printf ("Informe um numero: ");
+    scanf ("%lf", &num);
+
+
+    raiz = sqrt (num);
+
+
+    printf ("A raiz do numero eh: %.2lf.\n", raiz);
+
+
+    return 0;
+}
+*/
+/*
+int main ()
+{
+     double ye, x, res;
+
+    printf ("Informe um numero X: ");
+    scanf ("%lf", &x);
+
+    printf ("Informe um numero Y: ");
+    scanf ("%lf", &ye);
+
+    res = pow (x, ye);
+
+
+    printf (" O Numero elevado a %2.lf eh: %.2lf.",ye, res);
+
+    return 0;
+}
+*/
+
+
+/*
+int main ()
+{
+
+    double num1;
+
+    printf("Informe a nota do Aluno: ");
+    scanf ("%lf", &num1);
+
+    printf ("Floor: %.2lf \n", floor(num1));
+    printf ("Ceil: %.2lf \n", ceil(num1));
+    printf ("Round: %.2lf \n", round (num1));
+
+
+    return 0;
+}*/
+/*
+
+int main ()
+{
+     double x, cubo, quadrado;
+
+    printf ("Informe um numero X: ");
+    scanf ("%lf", &x);
+
+    quadrado = pow (x, 2);
+    cubo = pow (x,3);
+
+
+    printf ("Quadrado: %.2lf\nCubo: %.2lf", quadrado, cubo);
+
+    return 0;
+}
+*/
+
+/*
+int main ()
+{
+     double num, num2, raiz, soma;
+
+     printf ("Informe um numero:");
+     scanf ("%lf", &num);
+     printf ("Informe um numero:");
+     scanf ("%lf", &num2);
+
+     soma = num + num2;
+
+     raiz = sqrt (soma);
+
+     printf ("A Raiz quadrada do numer é: %.2lf", raiz);
+
+     return 0;
+
+
+}
+*/
+
+/*
+int main ()
+{
+    double termo, razao, posi, ordem;
+    printf ("informe um numero do termo:");
+    scanf ("%lf", &termo);
+    printf ("informe um numero da razao:");
+    scanf ("%lf", &razao);
+    printf ("informe um numero da posi:");
+    scanf ("%lf", &posi);
+
+    ordem = termo + (posi - 1) * razao;
+
+    printf ("O Calculo do termo informado foi: %.2lf", ordem);
+
+    return 0;
+}
+*/
+
+
+//resolver em casa
+int main ()
+
+{
+    double x1, x2, y1, y2, soma;
+    printf ("informe um numero x1:");
+    scanf ("%lf", &x1);
+    printf ("informe um numero x2:");
+    scanf ("%lf", &x2);
+    printf ("informe um numero y1:");
+    scanf ("%lf", &y1);
+    printf ("informe um numero y2:");
+    scanf ("%lf", &y2);
+
+
+    soma = sqrt ((pow = (x2 - x1),2) + (pow = (y2 - y1),2));
+
+    printf ("O Resultado da soma é: %.2lf", soma);
+
+    return 0;
+}
